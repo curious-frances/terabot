535270552758172fe5e4c678741316416128249b
Frances Raphael <francesraphael42@gmail.com>
2025-05-21 14:28:42 -0700

    modified -- compile issues

--- puppersim/deploy_to_robot.sh
+++ puppersim/deploy_to_robot.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  REMOTE=`avahi-resolve-host-name raspberrypi.local -4 | awk '{print $2}'`
+  REMOTE=$(avahi-resolve-host-name raspberrypi.local -4 | awk '{print $2}')
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  REMOTE=192.168.1.100 #raspberrypi.local
+  REMOTE=192.168.2.3 #raspberrypi.local
 else
   print "not supported os"
   exit 1
 fi
 
 echo PI addrees is "${REMOTE}"
-REMOTE_DIR=/home/pi/puppersim_deploy
+REMOTE_DIR=puppersim_deploy
 
-ssh pi@${REMOTE}  mkdir -p ${REMOTE_DIR}
-rsync -avz ${PWD} pi@${REMOTE}:${REMOTE_DIR}
+ssh frances@${REMOTE} mkdir -p ${REMOTE_DIR}
+rsync -avz ${PWD} frances@${REMOTE}:${REMOTE_DIR}
 
-if [ -z "$1" ] ; then
-  ssh -t pi@${REMOTE} "cd ${REMOTE_DIR} ; bash --login"
+if [ -z "$1" ]; then
+  ssh -t frances@${REMOTE} "cd ${REMOTE_DIR} ; bash --login"
 else
-  ssh -t pi@${REMOTE} "cd ${REMOTE_DIR} ; $@"
+  ssh -t frances@${REMOTE} "cd ${REMOTE_DIR} ; $@"
 fi
--- puppersim/puppersim/pupper_ars_run_policy.py
+++ puppersim/puppersim/pupper_ars_run_policy.py
@@ -2,22 +2,14 @@
 
 Code to load a policy and generate rollout data. Adapted from https://github.com/berkeleydeeprlcourse. 
 Example usage:
-python3 pupper_ars_run_policy.py --expert_policy_file=data/lin_policy_plus_best_10.npz --json_file=data/params.json
+python3 pupper_ars_run_policy.py --expert_policy_file=data/lin_policy_plus_latest.npz --json_file=data/params.json --render
 
 """
 import numpy as np
 import gym
 import time
-import pybullet_envs
-try:
-  import tds_environments
-except:
-  pass
-import json
-from arspb.policies import *
-import time
-import arspb.trained_policies as tp
 import os
+import json
 import pickle
 import puppersim
 import gin
@@ -25,6 +17,15 @@ from pybullet_envs.minitaur.envs_v2 import env_loader
 from pybullet import COV_ENABLE_GUI
 import puppersim.data as pd
 import argparse
+
+# Only import pybullet_envs if not running on robot
+import sys
+if '--run_on_robot' not in sys.argv:
+    import pybullet_envs
+    try:
+        import tds_environments
+    except:
+        pass
 
 def create_pupper_env(args):
     CONFIG_DIR = puppersim.getPupperSimPath()
@@ -41,12 +42,10 @@ def create_pupper_env(args):
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--expert_policy_file', type=str, default="data/lin_policy_plus_latest.npz",
-                      help='relative path to the policy weights. Defaults to where ars_train outputs weight file.')
-    parser.add_argument('--num_rollouts', type=int, default=20,
-                      help='Number of expert rollouts. Default is 20.')
-    parser.add_argument('--json_file', type=str, default="data/params.json",
-                      help='relative path to the policy parameters file. Defaults to where ars_train outputs params file.')
+    parser.add_argument('--expert_policy_file', type=str, required=True,
+                      help='Path to the policy file (.npz)')
+    parser.add_argument('--json_file', type=str, required=True,
+                      help='Path to the parameters JSON file')
     parser.add_argument('--run_on_robot', action='store_true',
                       help='whether to run the policy on the robot instead of in simulation. Default is False.')
     parser.add_argument('--render', default=False, action='store_true',
@@ -70,48 +69,15 @@ def main(argv):
     
     print("params=", params)
     
-    data = np.load(args.expert_policy_file, allow_pickle=True)
-
-    print('create gym environment:', params["env_name"])
+    # Create environment
     env = create_pupper_env(args)
-
-    lst = data.files
-    weights = data[lst[0]][0]
-    mu = data[lst[0]][1]
-    print("mu=", mu)
-    std = data[lst[0]][2]
-    print("std=", std)
-
-    ob_dim = env.observation_space.shape[0]
-    ac_dim = env.action_space.shape[0]
-    ac_lb = env.action_space.low
-    ac_ub = env.action_space.high
-
-    policy_params = {
-        'type': params["policy_type"],
-        'ob_filter': params['filter'],
-        'ob_dim': ob_dim,
-        'ac_dim': ac_dim,
-        'action_lower_bound': ac_lb,
-        'action_upper_bound': ac_ub,
-        'weights': weights,
-        'observation_filter_mean': mu,
-        'observation_filter_std': std
-    }
-
-    if params["policy_type"] == "nn":
-        print("FullyConnectedNeuralNetworkPolicy")
-        policy_sizes_string = params['policy_network_size_list'].split(',')
-        print("policy_sizes_string=", policy_sizes_string)
-        policy_sizes_list = [int(item) for item in policy_sizes_string]
-        print("policy_sizes_list=", policy_sizes_list)
-        policy_params['policy_network_size'] = policy_sizes_list
-        policy = FullyConnectedNeuralNetworkPolicy(policy_params, update_filter=False)
-    else:
-        print("LinearPolicy2")
-        policy = LinearPolicy2(policy_params, update_filter=False)
-    policy.get_weights()
-
+    obs_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    
+    # Load policy
+    policy = np.load(args.expert_policy_file)
+    w = policy['w']
+    
     returns = []
     observations = []
     actions = []
@@ -149,25 +115,32 @@ def main(argv):
                 print("loop dt:", time.time() - start_time_wall)
             start_time_wall = time.time()
             before_policy = time.time()
-            action = policy.act(obs)
+            
+            # Get action from policy
+            action = w.dot(obs)
+            
             after_policy = time.time()
 
             if not args.run_on_robot:
                 observations.append(obs)
                 actions.append(action)
 
-            obs, r, done, _ = env.step(action)
+            next_obs, r, done, _ = env.step(action)
+            
             if args.log_to_file:
                 log_dict['t'].append(env.robot.GetTimeSinceReset())
-                log_dict['MotorAngle'].append(obs[0:12])
-                log_dict['IMU'].append(obs[12:16])
+                log_dict['MotorAngle'].append(next_obs[0:12])
+                log_dict['IMU'].append(next_obs[12:16])
                 log_dict['action'].append(action)
 
             totalr += r
             steps += 1
+            obs = next_obs
+            
             if args.profile:
                 print('policy.act(obs): ', after_policy - before_policy)
                 print('wallclock_control_code: ', time.time() - start_time_wall)
+                
         returns.append(totalr)
     finally:
         if args.log_to_file:
